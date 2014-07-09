d862dbee481c4566f3edb439734be42806911960
Justin Veirs <justinveirs@gmail.com>
2014-07-09 08:19:17 -0600

    completed in 20 mintues

--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -9,7 +9,30 @@ require_relative 'fizz_buzz'
         expect(FizzBuzz.drink(1)).to eq 1
       end
 
-      
+      it 'takes the number 2 and returns 2' do
+        expect(FizzBuzz.drink(2)).to eq 2
+      end
+
+      it 'takes the number 3 and returns 3' do
+        expect(FizzBuzz.drink(3)).to eq 'Fizz'
+      end
+
+      it 'takes the number 4 and returns 4' do
+        expect(FizzBuzz.drink(4)).to eq 4
+      end
+
+      it 'takes the number 5 and returns Buzz' do
+        expect(FizzBuzz.drink(5)).to eq 'Buzz'
+      end
+
+      it 'takes the number 6, and returns Fizz' do
+        expect(FizzBuzz.drink(6)).to eq 'Fizz'
+      end
+
+      it 'takes the number 15 and returns FizzBuzz' do
+        expect(FizzBuzz.drink(15)).to eq 'FizzBuzz'
+      end
+
     end
 
   end
